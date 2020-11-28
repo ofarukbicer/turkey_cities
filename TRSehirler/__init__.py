44e355074de924257a49c7e3b2598654245747cc
keyiflerolsun <keyiflerolsun@gmail.com>
2020-11-29 00:12:36 +0300

    Gereksinimler Güncellendi

--- TRSehirler/__init__.py
+++ TRSehirler/__init__.py
@@ -5,13 +5,10 @@ YAZAR_POSTA = 'keyiflerolsun@gmail.com'
 
 PAKET       = 'TRSehirler'
 
-VERSIYON    = '0.1.1'
+VERSIYON    = '0.1.2'
 
 REPO        = 'https://github.com/keyiflerolsun/TRSehirler'
 ACIKLAMA    = 'Türkiye Cumhuriyeti Devleti Şehirlerini; İl, Plaka, Telefon veya İlçe bilgisinden bulun..'
 ANAHTAR_KLM = [PAKET, 'KekikAkademi', 'keyiflerolsun']
-
-with open(f'{PAKET}/requirements.txt') as dosya:
-    GEREKSINIM = dosya.read().splitlines()
 
 from TRSehirler.Sehirler import Sehir
